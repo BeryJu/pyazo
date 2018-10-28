75d0047455446065d974bba34a00020ba68f9cfe
Jens Langhammer <jens.langhammer@beryju.org>
2018-10-28 17:06:59 +0100

    ci: auto upload packages to nexus

---
+++ pyazo/management/commands/nexus_upload.py
@@ -0,0 +1,19 @@
+"""Pyazo nexus_upload management command"""
+import os
+from glob import glob
+
+import requests
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Upload debian package to nexus repository"""
+
+    def handle(self, *args, **options):
+        """Upload debian package to nexus repository"""
+        nexus_url = os.environ.get('NEXUS_URL')
+        nexus_user = os.environ.get('NEXUS_USER')
+        nexus_pass = os.environ.get('NEXUS_PASS')
+        print(requests.post('https://%s/repository/apt-test/' % nexus_url,
+                            data=open(glob('../pyazo-python3.5_*')[0], mode='rb'),
+                            auth=(nexus_user, nexus_pass)))
