2487674c84ceb58418ce32c3e58bfc8253bd496c
Jens Langhammer <jens.langhammer@beryju.org>
2020-02-01 23:16:49 +0100

    fix black formatting

--- pyazo/core/templatetags/gravatar.py
+++ pyazo/core/templatetags/gravatar.py
@@ -23,7 +23,7 @@ def gravatar(email, size=None, rating=None):
     """
     gravatar_url = "%savatar/%s" % (
         "https://secure.gravatar.com/",
-        md5(email.encode("utf-8")).hexdigest(), # nosec
+        md5(email.encode("utf-8")).hexdigest(),  # nosec
     )
 
     parameters = [p for p in (("s", size or "158"), ("r", rating or "g"),) if p[1]]
