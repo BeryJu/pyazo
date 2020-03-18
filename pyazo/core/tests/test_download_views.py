57219b9666579d8068222a5cde49ad591d4884c2
Jens Langhammer <jens.langhammer@beryju.org>
2020-03-18 09:57:07 +0100

    core: re-add clients to fix download in sidebar

--- pyazo/core/tests/test_download_views.py
+++ pyazo/core/tests/test_download_views.py
@@ -17,25 +17,12 @@ class DownloadViewTests(TestCase):
 
     def test_windows(self):
         """Test windows download"""
-        exe_path = os.path.join(settings.BASE_DIR + "/", "bin/", "Pyazo.exe")
-        with open(exe_path, "w") as _file:
-            _file.write("test")
         self.client.login(**test_auth())
         response = self.client.get(
             reverse("download-client", kwargs={"client": "windows"})
         )
         self.assertEqual(response["Content-Type"], "application/octet-stream")
         self.assertEqual(response.status_code, 200)
-
-    def test_windows_404(self):
-        """Test windows download but with missing file"""
-        exe_path = os.path.join(settings.BASE_DIR + "/", "bin/", "Pyazo.exe")
-        os.unlink(exe_path)
-        self.client.login(**test_auth())
-        response = self.client.get(
-            reverse("download-client", kwargs={"client": "windows"})
-        )
-        self.assertEqual(response.status_code, 404)
 
     def test_sharex(self):
         """Test shareX"""
@@ -47,22 +34,9 @@ class DownloadViewTests(TestCase):
 
     def test_macos(self):
         """Test macos download"""
-        exe_path = os.path.join(settings.BASE_DIR + "/", "bin/", "pyazo.dmg")
-        with open(exe_path, "w") as _file:
-            _file.write("test")
         self.client.login(**test_auth())
         response = self.client.get(
             reverse("download-client", kwargs={"client": "macos"})
         )
         self.assertEqual(response["Content-Type"], "application/x-apple-diskimage")
         self.assertEqual(response.status_code, 200)
-
-    def test_macos_404(self):
-        """Test macos download but with missing file"""
-        exe_path = os.path.join(settings.BASE_DIR + "/", "bin/", "pyazo.dmg")
-        os.unlink(exe_path)
-        self.client.login(**test_auth())
-        response = self.client.get(
-            reverse("download-client", kwargs={"client": "macos"})
-        )
-        self.assertEqual(response.status_code, 404)
